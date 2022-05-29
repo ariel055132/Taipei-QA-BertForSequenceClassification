95a3c5cb145ecc9057c6b8efbc09073e48b20f0e
Adrian Li <36882230+ariel055132@users.noreply.github.com>
2022-05-29 14:17:16 +0800

    Update crawlerTaipeiMTR.py

--- crawlers_dataprocessors/crawlerTaipeiMTR.py
+++ crawlers_dataprocessors/crawlerTaipeiMTR.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+department = ["臺北市政府捷運工程局"] * 157
+question = []
 
 url = "https://www.dorts.gov.taipei/News.aspx?n=2A66A485FACB0D5B&sms=87415A8B9CE81B16&page=1&PageSize=200"
 r = requests.get(url)
 html_soup = BeautifulSoup(r.text, 'html.parser')
 container = html_soup.find_all('div', class_="area-table rwd-straight")
+#container = html_soup.find_all(name= 'table', attrs={"id": "table_0"})
 for data in container:
-    print(data)
+    for tag in data.find_all('a'):
+        question.append(tag.text)
+        print(tag.text)
+
+result = pd.DataFrame(zip(department, question), columns=["部門", "問題"])
+result.to_csv("20220529_臺北市政府捷運工程局整理好QA.csv", sep=" ",index=False)
